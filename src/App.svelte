08503079872625be26571fdb71b0c7267e04542d
Caleb Herbison <caleb.h.herbison@gmail.com>
2020-08-22 12:32:36 -0600

    update some things

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,7 @@
 <script>
-   export let name;
+   window.onload = function() {
+      document.getElementById("year").innerHTML = new Date().getFullYear();
+   }
 </script>
 <header>
    <h1></h1>
@@ -17,17 +19,14 @@
    </div>
 </main>
 <footer class="container-fluid">
-   <div class="row">
-
-   </div>
 	<div class="row">
 		<div class="col" style="text-align: left;">
-			See this site's source code on <a href="https://github.com/timetravelingmoose/tourdesource.com">Github!</a>
+			See this site's source code on<a href="https://github.com/timetravelingmoose/tourdesource.com" class="fab fa-github-square fa-lg"></a>
 		</div>
 		<div class="col-6">
 		</div>
-		<div class="col" style="text-align: right;">
-			&#169; 2020 Caleb Herbison
+		<div class="col" style="text-align: right; padding-top: 5px;">
+			&#169; <span id="year"></span> Caleb Herbison
 		</div>
 	</div>
 </footer>
