04c1f871f9fb70aabac09c243a7ab17c18c35b8e
Caleb Herbison <caleb.h.herbison@gmail.com>
2020-08-22 19:49:42 -0600

    do some bootstrap things

--- src/App.svelte
+++ src/App.svelte
@@ -5,20 +5,14 @@
 </script>
 <main>
    <div class="container-fluid">
-      <header class="blog-header py-3">
-         <div class="row flex-nowrap justify-content-between align-items-center">
-            <div class="col-4 text-center">
-               <h1 class="blog-header-logo">Welcome to tour de ~/src!</h1>
-            </div>
-           <div class="col-4 text-center"></div>
+      <header class="blog-header">
+         <div class="nav-scroller py-1 mb-2">
+            <nav class="nav d-flex">
+               <a class="nav-link" href="#">Home</a>
+               <a class="nav-link" href="#about">About</a>
+            </nav>
          </div>
       </header>
-      <div class="nav-scroller py-1 mb-2">
-         <nav class="nav d-flex">
-            <a class="nav-link" href="#">Home</a>
-            <a class="nav-link" href="#about">About</a>
-         </nav>
-      </div>
       <div class="row content">
          <div class="col-sm-3 sidenav">
             <br>
@@ -30,14 +24,9 @@
    </div>
 </main>
 <footer class="container-fluid">
-	<div class="row">
-		<div class="col" style="text-align: left;">
-			See this site's source code on<a href="https://github.com/timetravelingmoose/tourdesource.com" class="fab fa-github-square fa-lg"></a>
-		</div>
-		<div class="col-6">
-		</div>
-		<div class="col" style="text-align: right; padding-top: 5px;">
-			&#169; <span id="year"></span> Caleb Herbison
+	<div class="row flex-nowrap justify-content-between align-items-center">
+		<div class="col" style="text-align:center">
+			<span class="text-nowrap">&#169; <span id="year"></span> Caleb Herbison</span> | <span class="text-nowrap" style="padding-right: 60px;"><a href="https://github.com/timetravelingmoose/tourdesource.com" class="fab fa-github-square fa-lg">Github</a></span>
 		</div>
 	</div>
 </footer>
