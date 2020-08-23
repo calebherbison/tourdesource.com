1e2c5a4c9d6f4e21fe0801a1de88407180447b01
Caleb Herbison <caleb.h.herbison@gmail.com>
2020-08-22 21:11:34 -0600

    fix typo

--- src/routes/About.svelte
+++ src/routes/About.svelte
@@ -1,4 +1,4 @@
 <div class="about-section">
   <h1>About me</h1>
-  <p>A profressional software engineer with a focus in software delivery and site reliability engineering.</p>
+  <p>A professional software engineer with a focus in software delivery and site reliability engineering.</p>
 </div>
